cd11abc2bd230628c370f26d26786f3e571502b2
Vinayak Kariappa Chettimada <vinayak.kariappa@gmail.com>
2015-10-10 05:56:14 +0200

    Working code to turn red LED on.

--- boot/build.sh
+++ boot/build.sh
@@ -4,3 +4,4 @@ arm-none-eabi-as -c -o start.o start.s
 arm-none-eabi-gcc -Os -mcpu=cortex-a7 -c -o boot.o boot.c
 arm-none-eabi-ld --script boot.lds -Map boot.map -o boot.elf start.o boot.o
 arm-none-eabi-size boot.elf
+arm-none-eabi-objcopy -O binary boot.elf boot.bin
--- boot/start.s
+++ boot/start.s
@@ -13,8 +13,21 @@ Redistribution and use in source and binary forms, with or without modification,
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-  .section .reset
-reset:
+/* Minimal head searched by BROM
+
+  .section .boot_head
+boot_head:
+  b reset
+  .ascii "eGON.BT0"
+  .word 0x5F0A6C39            // Initial stamp, updated by PC tool
+  .word 0                     // length, updated by PC tool
+  .word 0                     // pad to 32 bytes
+  .word 0                     //
+  .word 0                     //
+*/
+
+  .section .start
+start:
   mrs r0, cpsr
   bic r0, r0, #0x1F           // mask mode bits and uses little-endian
   orr r0, r0, #0xD3           // disable FIQ, IRQ and use SVC mode
